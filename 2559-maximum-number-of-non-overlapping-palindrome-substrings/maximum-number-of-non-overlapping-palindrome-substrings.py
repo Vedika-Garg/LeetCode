8b2a22d5ccd1c2342bdc65a32e3c309d534d183b
Vedika Garg <148520999+Vedika-Garg@users.noreply.github.com>
2026-09-15 23:47:38 +0530

    Time: 2984 ms (7.73%) | Memory: 19.5 MB (59.79%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -2,27 +2,72 @@ class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
         n = len(s)
 
-        # pal[i][j] = True if s[i:j+1] is a palindrome
-        pal = [[False] * n for _ in range(n)]
+        # Manacher: odd length palindromes
+        d1 = [0] * n
+        l = 0
+        r = -1
 
         for i in range(n):
-            pal[i][i] = True
+            if i > r:
+                j = 1
+            else:
+                j = min(d1[l + r - i], r - i + 1)
 
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
+            while i - j >= 0 and i + j < n and s[i - j] == s[i + j]:
+                j += 1
 
-                if s[i] == s[j] and (length == 2 or pal[i + 1][j - 1]):
-                    pal[i][j] = True
+            d1[i] = j
 
-        # dp[i] = maximum palindromes using first i characters
+            if i + j - 1 > r:
+                l = i - j + 1
+                r = i + j - 1
+
+        # Manacher: even length palindromes
+        d2 = [0] * n
+        l = 0
+        r = -1
+
+        for i in range(n):
+            if i > r:
+                j = 0
+            else:
+                j = min(d2[l + r - i + 1], r - i + 1)
+
+            while i - j - 1 >= 0 and i + j < n and s[i - j - 1] == s[i + j]:
+                j += 1
+
+            d2[i] = j
+
+            if i + j - 1 > r:
+                l = i - j
+                r = i + j - 1
+
+        # dp[i] = max palindromes using s[0:i]
         dp = [0] * (n + 1)
 
-        for i in range(1, n + 1):
-            dp[i] = dp[i - 1]
+        for end in range(n):
+            # Don't select a palindrome ending at end
+            dp[end + 1] = dp[end]
 
-            for j in range(i):
-                if i - j >= k and pal[j][i - 1]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+            for start in range(end + 1):
+                length = end - start + 1
+
+                if length < k:
+                    continue
+
+                # Check palindrome using Manacher
+                if length % 2 == 1:
+                    center = (start + end) // 2
+                    radius = length // 2 + 1
+
+                    if d1[center] >= radius:
+                        dp[end + 1] = max(dp[end + 1], dp[start] + 1)
+
+                else:
+                    center = (start + end + 1) // 2
+                    radius = length // 2
+
+                    if d2[center] >= radius:
+                        dp[end + 1] = max(dp[end + 1], dp[start] + 1)
 
         return dp[n]
